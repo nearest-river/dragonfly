84f1782503bec0069b76e95dab8a229a7f036663
Nate River <river177169@proton.me>
2026-06-06 14:41:14 +0530

    rm dead code

--- compiler/dragonfly-lexer/src/token_hint.rs
+++ compiler/dragonfly-lexer/src/token_hint.rs
@@ -33,21 +33,4 @@ impl TokenHint {
 impl TokenHintKind {
   pub const INFERRED_FLOAT: Self=Self::Float(None);
   pub const INFERRED_INT: Self=Self::Int(None);
-
-  #[inline]
-  pub const fn suffix_size_hint(&self)-> Option<usize> {
-    match self {
-      Self::Float(Some(kind))=> Some(kind.suffix_len()),
-      Self::Int(Some(kind))=> Some(kind.suffix_len()),
-      Self::Comment(kind)=> Some(kind.suffix_len()),
-      Self::Str(kind)=> Some(kind.suffix_len()),
-      Self::Char(kind)=> Some(kind.suffix_len()),
-      Self::Int(None)|Self::Float(None)=> None,
-      Self::RawLifetime=> None,
-      Self::Illegal(_)=> None,
-      Self::Lifetime=> None,
-      Self::RIdent=> None,
-      Self::Other=> None,
-    }
-  }
 }
