c6ce66d1ed02a036854c39f934fb626a4dcc2fb5
Nate River <river177169@proton.me>
2026-08-01 14:13:23 +0530

    impl source-map

--- compiler/dragonfly-span/src/lib.rs
+++ compiler/dragonfly-span/src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(internal_features)]
-#![feature(step_trait,rustc_attrs)]
+#![feature(step_trait,rustc_attrs,const_cmp,const_trait_impl,structural_match)]
 
 #[macro_use]
 mod macros;
@@ -7,17 +7,20 @@ mod macros;
 mod span;
 mod ident;
 mod symbol;
+mod source_map;
 
 pub use span::Span;
 pub use ident::Ident;
+pub use source_map::*;
 pub use symbol::{
   sym,
   Symbol,
 };
 
 
-pub struct SessionGlobals {
 
+pub struct SessionGlobals {
+  pub source_map: SourceMap,
 }
 
 
